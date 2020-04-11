f50edd386d3b80e37c9205c7efd1cddf7fea473e
yjc5726 <62366499+yjc5726@users.noreply.github.com>
2020-04-11 22:19:18 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,15 @@
   const hexColorBox = document.getElementById('hexColorBox')
 
   function rgbToHex() {
+    if (redInput.value === "" || greenInput.value === "" || blueInput.value === "") {
+      alert("請於欄位輸入0~255的數字，不可有空的欄位")
+    }
+    else {
+      hexProduce()
+    }
+  }
+
+  function hexProduce() {
     if (Number(redInput.value) > 255 || Number(greenInput.value) > 255 || Number(blueInput.value) > 255 || Number(redInput.value) < 0 || Number(greenInput.value) < 0 || Number(blueInput.value) < 0) {
       alert("數字超過0~255的範圍請重新輸入")
       redInput.value = ""
@@ -15,7 +24,6 @@
       hexColorCode.innerHTML = ""
       hexColorBox.style.backgroundColor = "white"
     }
-
     else if (Number(redInput.value) - Math.floor(Number(redInput.value)) > 0 || Number(greenInput.value) - Math.floor(Number(greenInput.value)) > 0 || Number(blueInput.value) - Math.floor(Number(blueInput.value)) > 0) {
       alert("數字必須要是整數，不能有小數")
       redInput.value = ""
@@ -31,7 +39,6 @@
       hexColorCode.innerHTML = `#${redValue.padStart(2, '0')}${greenValue.padStart(2, '0')}${blueValue.padStart(2, '0')}`
       hexColorBox.style.backgroundColor = `#${redValue.padStart(2, '0')}${greenValue.padStart(2, '0')}${blueValue.padStart(2, '0')}`
     }
-
   }
 
   button.addEventListener('click', rgbToHex)
